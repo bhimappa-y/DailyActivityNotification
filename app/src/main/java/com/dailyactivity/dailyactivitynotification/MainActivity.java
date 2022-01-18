6c2272e26845cca1a8da462642b999e5dc85f8d8
bhimappa-y <byalagudri123@gmail.com>
2022-01-18 23:28:46 +0530

    New page navigation added

--- app/src/main/java/com/dailyactivity/dailyactivitynotification/MainActivity.java
+++ app/src/main/java/com/dailyactivity/dailyactivitynotification/MainActivity.java
@@ -1,7 +1,10 @@
 package com.dailyactivity.dailyactivitynotification;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.new_activity);
+
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Create_Tasks.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
